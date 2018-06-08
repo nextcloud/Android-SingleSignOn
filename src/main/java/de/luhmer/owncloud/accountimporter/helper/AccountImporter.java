0382e67d098df97b895e134a557f3d43e94b8086
David Luhmer <david-dev@live.de>
2018-06-08 10:26:19 +0200

    Remove unused files/code

--- src/main/java/de/luhmer/owncloud/accountimporter/helper/AccountImporter.java
+++ src/main/java/de/luhmer/owncloud/accountimporter/helper/AccountImporter.java
@@ -20,8 +20,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-import de.luhmer.owncloud.accountimporter.interfaces.IAccountsReceived;
-
 import static android.content.Context.MODE_PRIVATE;
 
 /**
