ec9153c9fe8548e6e86d6f385856bef34c9d2b7b
David Luhmer <david-dev@live.de>
2018-06-15 14:52:45 +0200

    add missing header

--- src/main/java/de/luhmer/owncloud/accountimporter/ui/UiExceptionManager.java
+++ src/main/java/de/luhmer/owncloud/accountimporter/ui/UiExceptionManager.java
@@ -1,13 +1,32 @@
 package de.luhmer.owncloud.accountimporter.ui;
 
+import android.app.AlertDialog;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v4.app.NotificationCompat;
-import android.support.v7.app.AlertDialog;
 
 import de.luhmer.owncloud.accountimporter.exceptions.SSOException;
+
+/**
+ *  Nextcloud SingleSignOn
+ *
+ *  @author David Luhmer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 public class UiExceptionManager {
 
