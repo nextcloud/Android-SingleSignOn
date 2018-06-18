b302e17be2fb19951c9c81bfdb1eca3c8d77cf99
David Luhmer <david-dev@live.de>
2018-06-18 13:45:20 +0200

    add headers

--- src/main/java/de/luhmer/owncloud/accountimporter/helper/Retrofit2Helper.java
+++ src/main/java/de/luhmer/owncloud/accountimporter/helper/Retrofit2Helper.java
@@ -10,6 +10,25 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+/**
+ *  Nextcloud SingleSignOn
+ *
+ *  @author David Luhmer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 public class Retrofit2Helper {
 
     public static <T> Call<T> WrapInCall(final NextcloudAPI nextcloudAPI, final NextcloudRequest nextcloudRequest, final Type resType) {
