94bc955c612392fed6f531649d756516bf3a3313
Niedermann IT-Dienstleistungen <stefan-niedermann@users.noreply.github.com>
2020-01-19 18:51:06 +0100

    Add Context import statement
    Signed-off-by: David Luhmer <david-dev@live.de>

--- src/main/java/com/nextcloud/android/sso/helper/VersionCheckHelper.java
+++ src/main/java/com/nextcloud/android/sso/helper/VersionCheckHelper.java
@@ -1,6 +1,7 @@
 package com.nextcloud.android.sso.helper;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.util.Log;
