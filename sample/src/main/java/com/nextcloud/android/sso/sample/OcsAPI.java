16ed35137fa320a9e57272e98ddbee5a00042293
Stefan Niedermann <info@niedermann.it>
2021-11-04 09:58:17 +0100

    #395 Fix sample project
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- sample/src/main/java/com/nextcloud/android/sso/sample/OcsAPI.java
+++ sample/src/main/java/com/nextcloud/android/sso/sample/OcsAPI.java
@@ -14,14 +14,14 @@ import retrofit2.http.Path;
 public interface OcsAPI {
 
     @GET("capabilities?format=json")
-    Call<OcsResponse<OcsCapabilities>> getCapabilities();
+    Call<OcsResponse<OcsServerInfo>> getServerInfo();
 
     @GET("users/{search}?format=json")
     Call<OcsResponse<OcsUser>> getUser(@Path("search") String userId);
 
     /**
      * <p>A generic wrapper for <a href="https://www.open-collaboration-services.org/">OpenCollaborationServices</a> calls.</p>
-     * <p>This is needed for API endpoints located at <code>/ocs/...</code>. It is usually not used for APIs of 3rd party server apps like <a href="https://deck.readthedocs.io/en/latest/API/">Deck</a> or <a href="https://github.com/nextcloud/notes/blob/master/docs/api/README.md">Notes</a></p>
+     * <p>This is needed for API endpoints located at <code>/ocs/…</code>. It is usually not used for APIs of 3rd party server apps like <a href="https://deck.readthedocs.io/en/latest/API/">Deck</a> or <a href="https://github.com/nextcloud/notes/blob/master/docs/api/README.md">Notes</a></p>
      *
      * @param <T> defines the payload of this {@link OcsResponse}.
      */
@@ -40,31 +40,30 @@ public interface OcsAPI {
         }
     }
 
-    /*
-     * Extend the classes by the attributes you are actually using, for example:</p>
-     * <ul>
-     *     <li><code>version</code></li>
-     *     <li><code>theming</code></li>
-     *     <li><code>server_status</code></li>
-     *     <li><code>deck</code></li>
-     *     <li>…</li>
-     * </ul>
-     */
-    class OcsCapabilities {
-        public Theming theming;
+    class OcsServerInfo {
+        /* Extend the classes by the attributes you are actually using */
+        public OcsVersion version;
+        public OcsCapabilities capabilities;
 
-        static class Theming {
-            public String name;
+        static class OcsVersion {
+            /**
+             * You can map the node names to other variable names using {@link SerializedName}.
+             * See <a href="https://github.com/google/gson">Gson-</a> and <a href="https://square.github.io/retrofit/">Retrofit-</a>Documentation for all possibilities.
+             */
+            @SerializedName("string")
+            public String semanticVersion;
+        }
+
+        static class OcsCapabilities {
+            public Theming theming;
+
+            static class Theming {
+                public String name;
+            }
         }
     }
 
-    /**
-     * You can map the node names to other variable names using {@link SerializedName}.
-     * See <a href="https://github.com/google/gson">Gson-</a> and <a href="https://square.github.io/retrofit/">Retrofit-</a>Documentation for all possibilities.
-     */
     class OcsUser {
-        @SerializedName("id")
-        public String userId;
         @SerializedName("displayname")
         public String displayName;
     }
